42d3161c9495e74e3a08f675a2363de10cd70db5
Kevin Burleigh <klb@kindling>
2018-10-28 02:49:35 -0500

    fix bug in StateAction.operator<

--- search_problem.h
+++ search_problem.h
@@ -1,6 +1,10 @@
 #ifndef __SEARCH_PROBLEM_H__
 #define __SEARCH_PROBLEM_H__
 
+#include <iostream>
+#include <sstream>
+#include <string>
+
 #include <functional>
 #include <map>
 #include <optional>
@@ -9,169 +13,287 @@
 #include <vector>
 
 template<typename StateType, typename ActionType>
-class StateAction
-{
-public:
-  StateAction(const StateType& state, const ActionType& action)
-    : _state(state),
-      _action(action)
-  { }
-
-  bool operator< (const StateAction& other) const {
-    if (_state < other._state) {
-      return true;
-    }
-    else if (_action < other._action) {
-      return true;
-    }
-    else {
-      return false;
-    }
-  }
-
-private:
-  const StateType  _state;
-  const ActionType _action;
-};
-
-template <typename StateType, typename ActionType>
-class Solution
-{ };
-
-template<typename StateType, typename ActionType>
 class SearchProblem
 {
 public:
-  typedef StateAction<StateType,ActionType>                 StateActionType;
+  class StateAction;
+  class Builder;
+
+public:
   typedef std::tuple<StateType,ActionType,double,StateType> TransitionInfoType;
   typedef std::function<bool(const StateType&)>             GoalTestType;
-  typedef Solution<StateType,ActionType>                    SolutionType;
 
-  ///////////////////////////////////////
+  ///////////////////////////////////////////////////////////////////
   //
-  // Big-5
+  // inner class StateAction
   //
-  ///////////////////////////////////////
+  ///////////////////////////////////////////////////////////////////
 
 public:
-
-  SearchProblem ()                                    = default;
-  SearchProblem (const SearchProblem& orig)           = delete;
-  SearchProblem (const SearchProblem&& orig)          = delete;
-  SearchProblem& operator=(const SearchProblem& orig) = delete;
-  ~SearchProblem ()                                   = default;
-
-  ///////////////////////////////////////
-  //
-  // Public Interface
-  //
-  ///////////////////////////////////////
-
-public:
-
-  SearchProblem& addState (const StateType& state) {
-    _states.insert(state);
-
-    if (_actionsByState.count(state) == 0) {
-      std::set<ActionType> actions;
-      _actionsByState.insert({state,actions});
-    }
-
-    return *this;
-  }
-
-  SearchProblem& addStates (const std::vector<StateType>& states) {
-    for (auto state : states) {
-      this->addState(state);
-    }
-    return *this;
-  }
-
-  SearchProblem& addTransition (const TransitionInfoType& info)
+  class StateAction
   {
-    auto [state, action, cost, successor] = info;
+  public:
+    StateAction(const StateType& state, const ActionType& action)
+      : _state(state),
+        _action(action)
+    { }
 
-    //
-    // update states
-    //
-
-    this->addState(state);
-    this->addState(successor);
-
-    //
-    // update actions
-    //
-
-    auto pos = _actionsByState.find(state);
-    _actionsByState.at(state).insert(action);
-
-    StateAction stateAction {state, action};
-
-    //
-    // update costs
-    //
-
-    _costByStateAction.insert({stateAction,cost});
-
-    //
-    // update successors
-    //
-
-    _successorsByStateAction.insert({stateAction,successor});
-
-    return *this;
-  }
-
-  SearchProblem& addTransitions (const std::vector<TransitionInfoType>& infos) {
-    for (auto info : infos) {
-      this->addTransition(info);
+    bool operator== (const StateAction& other) const {
+      return (_state == other._state) && (_action == other._action);
     }
-    return *this;
-  }
 
-  SearchProblem& setStateState (const StateType& startState) {
-    this->addState(startState);
-    _startState = startState;
-    return *this;
-  }
-
-  SearchProblem& addGoalState (const StateType& goalState) {
-    this->addState(goalState);
-    _goalStates.insert(goalState);
-    return *this;
-  }
-
-  SearchProblem& addGoalStates (const std::vector<StateType>& goalStates) {
-    for (auto state : goalStates) {
-      this->addGoalState(state);
+    bool operator< (const StateAction& other) const {
+      if (_state < other._state) {
+        return true;
+      }
+      else if ((_state == other._state) && (_action < other._action)) {
+        return true;
+      }
+      else {
+        return false;
+      }
     }
-    return *this;
-  }
 
-  SearchProblem& addGoalTest (const GoalTestType& goalTest) {
-    _goalTests.push_back(goalTest);
-    return *this;
-  }
+    std::string toString () const {
+      std::ostringstream ostr;
+      ostr << "SA<" << _state << "," << _action << ">";
+      return ostr.str();
+    }
 
-  SolutionType solve () const {
+  private:
+    const StateType  _state;
+    const ActionType _action;
+  };
 
-    return SolutionType();
-  }
+  ///////////////////////////////////////////////////////////////////
+  //
+  // inner class Builder
+  //
+  ///////////////////////////////////////////////////////////////////
+
+public:
+
+  class Builder
+  {
+  public:
+
+    ///////////////////////////////////////
+    // Big-5
+    ///////////////////////////////////////
+
+  public:
+    Builder ()                              = default;
+    Builder (const Builder& orig)           = delete;
+    Builder (Builder&& orig)                = delete;
+    Builder& operator=(const Builder& orig) = delete;
+    ~Builder ()                             = default;
+
+    ///////////////////////////////////////
+    // Public Interface
+    ///////////////////////////////////////
+
+  public:
+
+    Builder& addState (const StateType& state) {
+      _states.insert(state);
+
+      if (_actionsByState.count(state) == 0) {
+        std::set<ActionType> actions;
+        _actionsByState.insert({state,actions});
+      }
+
+      return *this;
+    }
+
+    Builder& addStates (const std::vector<StateType>& states) {
+      for (auto state : states) {
+        this->addState(state);
+      }
+      return *this;
+    }
+
+    Builder& addTransition (const TransitionInfoType& info) {
+      auto [state, action, cost, successor] = info;
+
+      // update states
+      this->addState(state);
+      this->addState(successor);
+
+      // update actions
+      auto pos = _actionsByState.find(state);
+      _actionsByState.at(state).insert(action);
+
+      StateAction stateAction {state, action};
+
+      // update costs
+      _costByStateAction.insert({stateAction,cost});
+
+      // update successors
+      _successorByStateAction.insert({stateAction,successor});
+
+      return *this;
+    }
+
+    Builder& addTransitions (const std::vector<TransitionInfoType>& infos) {
+      for (auto info : infos) {
+        this->addTransition(info);
+      }
+      return *this;
+    }
+
+    Builder& setStateState (const StateType& startState) {
+      this->addState(startState);
+      _startState = startState;
+      return *this;
+    }
+
+    Builder& addGoalState (const StateType& goalState) {
+      this->addState(goalState);
+      _goalStates.insert(goalState);
+      return *this;
+    }
+
+    Builder& addGoalStates (const std::vector<StateType>& goalStates) {
+      for (auto state : goalStates) {
+        this->addGoalState(state);
+      }
+      return *this;
+    }
+
+    Builder& addGoalTest (const GoalTestType& goalTest) {
+      _goalTests.push_back(goalTest);
+      return *this;
+    }
+
+    SearchProblem build () const {
+      if (!_startState.has_value()) {
+        throw std::logic_error("start state must be specified");
+      }
+
+      SearchProblem result(
+        _states,
+        _actionsByState,
+        _costByStateAction,
+        _successorByStateAction,
+        *_startState,
+        _goalStates,
+        _goalTests
+      );
+
+      return result;
+    }
+
+    ///////////////////////////////////////
+    // Private Attributes
+    ///////////////////////////////////////
+
+  private:
+    std::set<StateType>                      _states;
+    std::map<StateType,std::set<ActionType>> _actionsByState;
+    std::map<StateAction,double>             _costByStateAction;
+    std::map<StateAction,StateType>          _successorByStateAction;
+    std::optional<StateType>                 _startState;
+    std::set<StateType>                      _goalStates;
+    std::vector<GoalTestType>                _goalTests;
+  };
+
+  ///////////////////////////////////////////////////////////////////
+  //
+  // class SearchProblem
+  //
+  ///////////////////////////////////////////////////////////////////
 
   ///////////////////////////////////////
-  //
-  // Private Attributes
-  //
+  // Big-5
   ///////////////////////////////////////
 
 private:
-  std::set<StateType>                      _states;
-  std::map<StateType,std::set<ActionType>> _actionsByState;
-  std::map<StateActionType,double>         _costByStateAction;
-  std::map<StateActionType,StateType>      _successorsByStateAction;
-  std::optional<StateType>                 _startState;
-  std::set<StateType>                      _goalStates;
-  std::vector<GoalTestType>                _goalTests;
+  SearchProblem (const std::set<StateType>&                      states,
+                 const std::map<StateType,std::set<ActionType>>& actionsByState,
+                 const std::map<StateAction,double>&             costByStateAction,
+                 const std::map<StateAction,StateType>&          successorsByStateAction,
+                 const StateType&                                startState,
+                 const std::set<StateType>&                      goalStates,
+                 const std::vector<GoalTestType>&                goalTests)
+    : _states(states),
+      _actionsByState(actionsByState),
+      _costByStateAction(costByStateAction),
+      _successorByStateAction(successorsByStateAction),
+      _startState(startState),
+      _goalStates(goalStates),
+      _goalTests(goalTests)
+  { }
+
+public:
+  SearchProblem (const SearchProblem& orig)             = default;
+  SearchProblem (SearchProblem&& orig)                  = delete;
+  SearchProblem& operator= (const SearchProblem& other) = default;
+  ~SearchProblem ()                                     = default;
+
+  ///////////////////////////////////////
+  // Public interface
+  ///////////////////////////////////////
+
+public:
+  const std::set<StateType>& getStates () const {
+    return _states;
+  }
+
+  const StateType& getStartState () const {
+    return _startState;
+  }
+
+  const std::set<ActionType>& getActionsForState(const StateType& state) const {
+    return _actionsByState.at(state);
+  }
+
+  const StateType& getActionSuccessor(const StateType& state, const ActionType& action) const {
+    StateAction stateAction(state, action);
+    return _successorByStateAction.at(stateAction);
+  }
+
+  double getActionCost(const StateType& state, const ActionType& action) const {
+    StateAction stateAction(state, action);
+    return _costByStateAction.at(stateAction);
+  }
+
+  bool isGoal (const StateType& state) const {
+    bool result = false;
+
+    if (!result) {
+      result = result || (_goalStates.count(state) != 0);
+    }
+
+    if (!result) {
+      for (auto goalTest : _goalTests) {
+        result = result || goalTest(state);
+        if (result) {
+          break;
+        }
+      }
+    }
+
+    return result;
+  }
+
+  ///////////////////////////////////////
+  // Private Attributes
+  ///////////////////////////////////////
+
+private:
+  const std::set<StateType>                      _states;
+  const std::map<StateType,std::set<ActionType>> _actionsByState;
+  const std::map<StateAction,double>             _costByStateAction;
+  const std::map<StateAction,StateType>          _successorByStateAction;
+  const StateType                                _startState;
+  const std::set<StateType>                      _goalStates;
+  const std::vector<GoalTestType>                _goalTests;
 };
+
+template <typename StateType, typename ActionType>
+std::ostream& operator<< (std::ostream& ostr, const typename SearchProblem<StateType,ActionType>::StateAction& sa) {
+  ostr << sa.toString();
+  return ostr;
+}
 
 #endif // __SEARCH_PROBLEM_H__
--- test_search_problem.cc
+++ test_search_problem.cc
@@ -1,37 +1,55 @@
 #include "search_problem.h"
 
+#include <iostream>
 #include <functional>
 #include <string>
 
 using namespace std;
 
 int main () {
-  SearchProblem<int,string> searchProblem;
+  SearchProblem<int,string>::Builder spBuilder;
 
   SearchProblem<int,string>::GoalTestType test1 = [] (const int& state) {
     return state == 2;
   };
 
-  searchProblem.addTransitions({
-                  {1, "goto2", 1.0, 2},
-                  {1, "goto3", 2.0, 3},
-                  {1, "goto4", 3.0, 4},
-                  {1, "goto5", 4.0, 5}
-                })
-                .addTransitions({
-                  {2, "goto5", 3.0, 5},
-                  {2, "goto6", 4.0, 6},
-                  {3, "goto2", 1.0, 2}
-                })
-                .addState(7)
-                .addGoalTest(test1)
-                .addTransition({4, "goto4", 0.1, 4})
-                .setStateState(9)
-                .addGoalState(5)
-                .addStates({8,9,10})
-                .addGoalState(15)
-                .setStateState(10)
-                .addTransition({5, "goto1", 4.0, 1});
+  spBuilder
+    .addTransitions({
+      {1, "goto2", 1.0, 2},
+      {1, "goto3", 2.0, 3},
+      {1, "goto4", 3.0, 4},
+      {1, "goto5", 4.0, 5}
+    })
+    .addTransitions({
+      {2, "goto5", 3.0, 5},
+      {2, "goto6", 4.0, 6},
+      {3, "goto2", 1.0, 2},
+      {3, "goto2", 1.0, 10}
+    })
+    .addTransition({5, "goto1", 4.0, 1})
+    .addState(7)
+    .addGoalTest(test1)
+    .setStateState(9)
+    .addTransition({4, "goto4", 0.1, 4})
+    .addGoalState(5)
+    .addStates({8,9,10})
+    .addGoalState(15)
+    .setStateState(10);
 
-  auto solution = searchProblem.solve();
+  auto searchProblem = spBuilder.build();
+
+
+  for (auto state : searchProblem.getStates()) {
+    cout << "state: " << state << endl;
+    cout << "  actions:" << endl;
+    for (auto action : searchProblem.getActionsForState(state)) {
+      cout << "    action: " << action << endl;
+
+      auto successor = searchProblem.getActionSuccessor(state, action);
+      cout << "      successor: " << successor << endl;
+
+      auto cost = searchProblem.getActionCost(state, action);
+      cout << "      cost: " << cost << endl;
+    }
+  }
 }
